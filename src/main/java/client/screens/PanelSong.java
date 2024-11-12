3da7cf64cea02fbfae91f666897c3fd90ad7752e
Evgeny Mironov <e.d.mironov@yandex.ru>
2024-11-12 23:17:27 +0300

    Добавлена абстрактная модель таблицы

--- src/main/java/client/screens/PanelSong.java
+++ src/main/java/client/screens/PanelSong.java
@@ -5,8 +5,12 @@ import client.ClientManager;
 import client.utils.Utils;
 
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 
 /**
@@ -17,6 +21,8 @@ public class PanelSong {
     private static JPanel panel = new JPanel();
     private static String[] columnsName = new String[]{"Исполнитель", "Название песни", "Ссылка"};
     private static String[][] songs = new ClientManager().getSongs();
+    protected final SongTableModel tableModel = new SongTableModel();
+
 
     public JPanel getPanel() {
         return showPanelSong(songs, columnsName);
@@ -28,6 +34,40 @@ public class PanelSong {
         panel.add(backButton);
         JTable tableSong = new JTable(song, columnsName);
         tableSong.setRowHeight(80);
+        tableSong.setFillsViewportHeight(true);
+        tableSong.setShowGrid(true);
+        tableSong.setGridColor(Color.RED);
+        tableSong.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Просто клик по строке");
+                if (e.getClickCount() == 2 && e.getButton() == MouseEvent.BUTTON1) {
+                    System.out.println("Двойной клик по строке");
+
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
         panel.add(new JScrollPane(tableSong));
 
         return panel;
@@ -40,5 +80,34 @@ public class PanelSong {
             new Utils().openMainPanel();
             System.out.println("Нажали на кнопку возврата на главную страницу");
         }
+    }
+
+    public class SongTableModel extends AbstractTableModel {
+        private final String[] columnsName = {"Исполнитель", "Название песни", "Ссылка"};
+        private Object[][] songs = new ClientManager().getSongs();
+
+        @Override
+        public int getRowCount() {
+            return songs.length;
+        }
+
+        @Override
+        public int getColumnCount() {
+            return columnsName.length;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            return null;
+        }
+
+        @Override
+        public boolean isCellEditable(int row, int column) { return false; }
+
+        public void setValueAt(Object value, int row, int col) {
+            songs[row][col] = value;
+            fireTableCellUpdated(row, col);
+        }
+
     }
 }
